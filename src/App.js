07c650fd9e64a8f624d31193b3f30e2defe61538
ContractConnect <Aarondouglasfox@gmail.com>
2025-07-06 03:35:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import ClientInfoForm from './components/ClientInfoForm';
-
-function App() {
-  return <ClientInfoForm />;
-}
-
-export default App;
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './components/HomePage';
 import ClientInfoForm from './components/ClientInfoForm';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ClientInfoForm />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/form" element={<ClientInfoForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
-import HomePage from './components/HomePage';
 
-<Route path="/" element={<HomePage />} />
-<Route path="/form" element={<ClientInfoForm />} />
+export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Dashboard (Coming Soon)</h2>
+    </div>
+  );
+}
