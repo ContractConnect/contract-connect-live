153e4b581c6f571c74359f4854aba7acd2a1502f
ContractConnect <Aarondouglasfox@gmail.com>
2025-07-06 00:33:02 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ClientInfoForm from './components/ClientInfoForm';
-export default function App() {
+
+function App() {
   return <ClientInfoForm />;
 }
+
+export default App;
