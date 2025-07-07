aab02d39738468a86ca7a8fc40b794f8b6925dad
ContractConnect <Aarondouglasfox@gmail.com>
2025-07-07 14:07:29 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from './components/HomePage';
 import ClientInfoForm from './components/ClientInfoForm';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/form" element={<ClientInfoForm />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
+  return <ClientInfoForm />;
 }
 
 export default App;
